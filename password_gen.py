34067274e39f04ce7dc3824ca44706dcfd8e6b2f
Pavankalyan5116 <pawankalyanvarikuppala2276@gmail.com>
2025-07-28 21:43:34 +0530

    Add files via upload

---
+++ password_gen.py
@@ -0,0 +1,23 @@
+#we need to generate a password randomly in shuffle
+import string
+import random as r
+letters = string.ascii_uppercase + string.ascii_lowercase +string.digits + string.punctuation
+
+print("welcome to Password Generator")
+l = int(input("enter length of password: "))
+
+li=[]
+for i in range(l+1):
+    if l>=8:
+        char = r.choice(letters)
+        li += char
+    else:
+        print("Reqiured minimum 8 characters")
+        break
+
+r.shuffle(li)
+le=""
+for i in li:
+    le +=i
+print(le)
+
